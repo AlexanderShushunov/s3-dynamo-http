53ebd5cd58f5916497e9a0f350d78ee7e5e071e5
AlexanderShushunov <AShushunov@mail.ru>
2021-03-03 15:05:40 +0300

    chore: migrate handler to es-modules

--- src/handlers/s3-to-dynamo.ts
+++ src/handlers/s3-to-dynamo.ts
@@ -3,7 +3,7 @@ import { S3 } from 'aws-sdk'
 
 const s3 = new S3()
 
-exports.s3ToDynamoHandler = async (event: S3CreateEvent) => {
+export const s3ToDynamoHandler = async (event: S3CreateEvent) => {
   // All log statements are written to CloudWatch by default. For more information, see
   // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
   const getObjectRequests = event.Records.map(record => {
